1ea815f506e20c6c5dffad5230d71a229ece7ea6
Alexandr Novikov <itiggix@gmail.com>
2021-11-15 07:04:35 +0300

    Added roles

---
+++ Fireguard/Core/Domain/Roles/Role.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Domain.Roles
+{
+    public class Role : BaseEntity
+    {
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Тип роли
+        /// </summary>
+        public RoleType Type { get; set; }
+    }
+}
---
+++ Fireguard/Core/Domain/Roles/RoleType.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Domain.Roles
+{
+    public enum RoleType
+    {
+        /// <summary>
+        /// Гость
+        /// </summary>
+        Guest = 0,
+
+        /// <summary>
+        /// Авторизированный пользователь
+        /// </summary>
+        User = 1,
+
+        /// <summary>
+        /// Администратор
+        /// </summary>
+        Administrator = 2
+    }
+}
